d96624a392875fd8515751372a78aa0162a767a1
= <abidtkg@gmail.com>
2026-05-25 13:06:05 +0600

    feat: add admin authentication routes and models with Swagger documentation

---
+++ configs/AdminTokenVerify.config.js
@@ -0,0 +1,13 @@
+const JWT = require('jsonwebtoken');
+module.exports = function(req, res, next){
+    const token = req.header('token');
+    if(!token) return res.status(401).json({error: 'Access Denied'});
+
+    try{
+        const verified = JWT.verify(token, process.env.ADMIN_JWT_SECRET);
+        req.user = verified;
+        next();
+    }catch(error){
+        return res.status(403).json({error: 'Invalid Token'});
+    }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,8 @@ const AuthValidator = require('../validators/auth.validator');
 const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
 const User = require('../models/User.model');
-
+const Admin = require('../models/Admin.model');
+const AdminTokenVerify = require('../configs/AdminTokenVerify.config');
 
 /**
 * @swagger
@@ -68,10 +69,60 @@ router.post('/login', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(401).json({ error: 'Invalid email or password' });
 
-    const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+    const token = JWT.sign({ id: user._id }, process.env.USER_JWT_SECRET, { expiresIn: process.env.USER_JWT_EXPIRES_IN });
     return res.status(200).json({ token: token, name: user.name, email: user.email });
 });
 
+
+/**
+* @swagger
+*   /auth/register:
+*   post:
+*       description: Register a new user account
+*       summary: 
+*       tags:
+*           - Authentication
+*       responses:
+*           '200':
+*               description: User registered successfully.
+*               schema:
+*                 type: object
+*                 properties:
+*                   name:
+*                       type: string
+*                   email:
+*                       type: string
+*                   token:
+*                       type: string
+
+*           '400':
+*               description: Bad Request
+*               schema:
+*                 type: object
+*                 properties:
+*                   error:
+*                       type: string
+*           '500':
+*               description: Internal Server Error
+*               schema:
+*                   type: object
+*                   properties:
+*                       error:
+*                           type: string
+*       parameters:
+*         - in: body
+*           name: user
+*           description: User login details
+*           schema:
+*              type: object
+*              properties:
+*                  name:
+*                      type: string
+*                  email:
+*                      type: string
+*                  password:
+*                      type: string
+*/
 router.post('/register', async (req, res) => {
     const { error } = AuthValidator.registerValidator(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
@@ -97,6 +148,154 @@ router.post('/register', async (req, res) => {
     try {
         await newUser.save();
         return res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        return res.status(500).json({ error: '500 Internal Server Error' });
+    }
+});
+
+
+/**
+* @swagger
+*   /auth/admin/login:
+*   post:
+*       description: Login to an admin account
+*       summary: 
+*       tags:
+*           - Authentication
+*       responses:
+*           '200':
+*               description: Admin logged in successfully.
+*               schema:
+*                 type: object
+*                 properties:
+*                   name:
+*                       type: string
+*                   email:
+*                       type: string
+*                   token:
+*                       type: string
+*           '400':
+*               description: Bad Request
+*               schema:
+*                 type: object
+*                 properties:
+*                   error:
+*                       type: string
+*           '500':
+*               description: Internal Server Error
+*               schema:
+*                   type: object
+*                   properties:
+*                       error:
+*                           type: string
+*       parameters:
+*         - in: body
+*           name: user
+*           description: User login details
+*           schema:
+*              type: object
+*              properties:
+*                  email:
+*                      type: string
+*                  password:
+*                      type: string
+*/
+router.post('/admin/login', async (req, res) => {
+    const { error } = AuthValidator.loginValidator(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    let user;
+    try {
+        user = await Admin.findOne({ email: req.body.email });
+    } catch (error) {
+        return res.status(500).json({ error: '500 Internal Server Error' });
+    }
+
+    if (!user) return res.status(401).json({ error: 'Invalid email or password' });
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(401).json({ error: 'Invalid email or password' });
+
+    const token = JWT.sign({ id: user._id }, process.env.ADMIN_JWT_SECRET, { expiresIn: process.env.ADMIN_JWT_EXPIRES_IN });
+    return res.status(200).json({ token: token, name: user.name, email: user.email });
+});
+
+
+/**
+* @swagger
+*   /auth/admin/register:
+*   post:
+*       description: Register a new admin account
+*       summary: 
+*       tags:
+*           - Authentication
+*       security:
+*           - APIKeyHeader: []
+*       responses:
+*           '200':
+*               description: Admin registered successfully.
+*               schema:
+*                 type: object
+*                 properties:
+*                   name:
+*                       type: string
+*                   email:
+*                       type: string
+*                   token:
+*                       type: string
+
+*           '400':
+*               description: Bad Request
+*               schema:
+*                 type: object
+*                 properties:
+*                   error:
+*                       type: string
+*           '500':
+*               description: Internal Server Error
+*               schema:
+*                   type: object
+*                   properties:
+*                       error:
+*                           type: string
+*       parameters:
+*         - in: body
+*           name: user
+*           description: Admin login details
+*           schema:
+*              type: object
+*              properties:
+*                  name:
+*                      type: string
+*                  email:
+*                      type: string
+*                  password:
+*                      type: string
+*/
+router.post('/admin/register', AdminTokenVerify, async (req, res) => {
+    const { error } = AuthValidator.registerValidator(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    let user;
+    try {
+        user = await Admin.findOne({ email: req.body.email });
+    } catch (error) {
+        return res.status(500).json({ error: '500 Internal Server Error' });
+    }
+
+    if (user) return res.status(400).json({ error: 'Email already exists' });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new Admin({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+
+    try {
+        await newUser.save();
+        return res.status(201).json({ message: 'Admin registered successfully' });
     } catch (error) {
         return res.status(500).json({ error: '500 Internal Server Error' });
     }
